f80e7a3b1ce21a2b96bb06528a9a08ac72493630
Malcolm Abbott <mjacoding@gmail.com>
2023-09-19 00:12:59 +0000

    Submit event listener to the form: no refresh, stores input values in object. Issue #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-const $photoUrl = document.querySelector('input[name="photo"]');
-$photoUrl.addEventListener('input', (event) => {
+const $photo = document.querySelector('#photo');
+$photo.addEventListener('input', (event) => {
   const $imgCreate = document.querySelector('.create');
-  $imgCreate.setAttribute('src', $photoUrl.value);
+  $imgCreate.setAttribute('src', $photo.value);
+});
+
+const $form = document.querySelector('form');
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const $title = document.querySelector('#title');
+  const $notes = document.querySelector('#notes');
+  const values = {
+    title: $title.value,
+    photo: $photo.value,
+    notes: $notes.value,
+  };
+  values.deleteThis = 'BEFORE CONTINUING';
 });
