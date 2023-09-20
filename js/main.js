068c233fefb43fb6131df9031d2b8a328aa6acb0
Malcolm Abbott <mjacoding@gmail.com>
2023-09-20 18:03:07 +0000

    Define a function which generates a dom tree for each data.entries object as an li element. Issue #2

--- js/main.js
+++ js/main.js
@@ -25,3 +25,31 @@ const $entry = document.querySelector('div[data-view="entry-form"]');
 const $entries = document.querySelector('div[data-view="entries"]');
 $entry.classList.add('hidden');
 $entries.classList.remove('hidden');
+
+function renderEntry(entry) {
+  const $li = document.createElement('li');
+  const $row = document.createElement('div');
+  $row.className = 'row';
+  $li.append($row);
+  const $half = document.createElement('div');
+  $half.className = 'column-half';
+  $row.append($half);
+  const $imgWrapper = document.createElement('div');
+  $imgWrapper.className = 'img-wrapper';
+  $half.append($imgWrapper);
+  const $imgEntries = document.createElement('img');
+  $imgEntries.setAttribute('src', entry.photo);
+  $imgWrapper.append($imgEntries);
+  const $half2 = document.createElement('div');
+  $half2.className = 'column-half';
+  $row.append($half2);
+  const $h3 = document.createElement('h3');
+  $h3.textContent = entry.title;
+  $half2.append($h3);
+  const $p = document.createElement('p');
+  $p.textContent = entry.notes;
+  $half2.append($p);
+  return $li;
+}
+
+renderEntry(data.entries[0]);
